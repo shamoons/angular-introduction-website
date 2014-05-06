921aea33f4d84c38fca87a0342423cc023004356
Shamoon Siddiqui <ssiddiqui@liquidnet.com>
2014-05-06 15:20:19 -0400

    Calculating the breadcrumb'ed path

--- public/scripts/controllers/source.js
+++ public/scripts/controllers/source.js
@@ -3,7 +3,14 @@
 angularMoonApp.controller('SourceController', ['$scope', '$routeParams', 'fileService', function ($scope, $routeParams, fileService) {
 	fileService.getContents($routeParams.path).then(function(response) {
 		$scope.contents = response.data;
-		$scope.breadcrumbPath = response.data[0].path.split('/');
+		if($scope.contents.length > 1) {
+			$scope.breadcrumbPath = response.data[0].path.split('/');	
+		} else {
+			$scope.breadcrumbPath = response.data.path.split('/');
+			$scope.breadcrumbPath.push('');
+		}
+		
+		console.log($scope.breadcrumbPath);
 
 	});
 }]);
