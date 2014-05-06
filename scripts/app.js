7926a2dfc5f4edfb4bc1e4c78d58aab777c02c37
Shamoon Siddiqui <ssiddiqui@liquidnet.com>
2014-05-06 10:36:43 -0400

    Added an express server

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,17 @@
 'use strict';
 var angularMoonApp = angular.module('angularMoon', [
 	'ngRoute'
-]).config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+]).config(['$routeProvider, $locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider
+    .when('/', {
+      templateUrl: 'templates/home.html'
+    })
+    .when('/search', {
+      templateUrl: 'templates/search.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+    $locationProvider.html5Mode(true);
 }]);
